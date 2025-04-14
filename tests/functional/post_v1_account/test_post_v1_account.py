53eb8443540f3a071e41a9283d3fde25e94aee2d
LezovOzon <allezov@ozon.ru>
2025-04-15 00:39:14 +0700

    Добавил конфигуратор, вынес функцию активации токена в отдельный модуль(draft)

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,17 +1,21 @@
-from json import loads
-
 from dm_api_account.apis.account_api import AccountApi
 from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
+from restclient.configuration import Configuration as MailhogConfiguration
+from restclient.configuration import Configuration as DmApiConfiguration
+from helper import helper_draft
 
 
 def test_post_v1_account():
-    account_api = AccountApi(host='http://5.63.153.31:5051')
-    login_api = LoginApi(host='http://5.63.153.31:5051')
-    mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=False)
+
+    account_api = AccountApi(dm_api_configuration)
+    login_api = LoginApi(dm_api_configuration)
+    mailhog_api = MailhogApi(mailhog_configuration)
 
     # Регистрация
-    login = f'allezov15'
+    login = f'allezov17'
     email = f'{login}@mail.ru'
     password = '123123123'
 
@@ -29,7 +33,7 @@ def test_post_v1_account():
     assert response.status_code == 200, f" Письма не получены {response.json()}"
 
     # Получить активационный токен
-    token = get_activation_token_by_login(login=login, response=response)
+    token = helper_draft.get_activation_token_by_login(login=login, response=response)
     assert token is not None, f"Токен для пользователя {login}, не был получен"
 
     # Активация
@@ -39,23 +43,3 @@ def test_post_v1_account():
     # Авторизация
     response = login_api.post_v1_account_login(json_data=json_data)
     assert response.status_code == 200, f" Не удалось авторизовать пользователя {response.json()}"
-
-
-def get_activation_token_by_login(
-        login,
-        response
-):
-    """
-    Get token from emailmessage
-    :param login:
-    :param response:
-    :return:
-    """
-    token = None
-    for message in response.json()['items']:
-        user_data = loads(message['Content']['Body'])
-        user_login = user_data['Login']
-        if user_login == login:
-            token = user_data['ConfirmationLinkUrl'].split('/')[-1]
-
-    return token
