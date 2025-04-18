b5902fb4645b65a304be885ee00be1b7ea05df64
LezovOzon <allezov@ozon.ru>
2025-04-18 13:07:05 +0700

    добавлены фикстуры для клиентов и подготовки тест пользователя

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,3 +1,8 @@
+from collections import namedtuple
+from datetime import datetime
+
+import pytest
+
 from restclient.configuration import Configuration as MailhogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
 from services.dm_api_account import DmApiAccount
@@ -16,18 +21,48 @@ structlog.configure(
 )
 
 
-def test_post_v1_account():
-    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+@pytest.fixture
+def account_api():
     dm_api_configuration = DmApiConfiguration(host='http://5.63.153.31:5051', disable_log=True)
+    account_client = DmApiAccount(configuration=dm_api_configuration)
+    return account_client
 
-    account = DmApiAccount(configuration=dm_api_configuration)
-    mailhog = MailHogApi(configuration=mailhog_configuration)
-    account_helper = AccountHelper(dm_account_api=account, mailhog=mailhog)
 
-    # Регистрация
-    login = f'allezov95'
+@pytest.fixture
+def mailhog_api():
+    mailhog_configuration = MailhogConfiguration(host='http://5.63.153.31:5025')
+    mailhog_client = MailHogApi(configuration=mailhog_configuration)
+    return mailhog_client
+
+
+@pytest.fixture()
+def account_helper(
+        account_api,
+        mailhog_api
+):
+    account_helper = AccountHelper(dm_account_api=account_api, mailhog=mailhog_api)
+    return account_helper
+
+
+@pytest.fixture()
+def prepare_test_user():
+    now = datetime.now()
+    data = now.strftime("%d_%m_%Y_%H_&M_%S")
+    login = f'allezov{data}'
     email = f'{login}@mail.ru'
     password = '123123123'
+    User = namedtuple("User", ['login', 'password', 'email'])
+    user = User(login=login, password=password, email=email)
+    return user
+
+
+def test_post_v1_account(
+        account_helper,
+        prepare_test_user
+):
+    login = prepare_test_user.login
+    password = prepare_test_user.password
+    email = prepare_test_user.email
 
     account_helper.register_new_user(login=login, email=email, password=password)
     response = account_helper.user_login(login=login, password=password)
