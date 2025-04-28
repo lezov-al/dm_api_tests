234a4a6c7f7d22c83f949cf13b194996399eb11c
LezovOzon <allezov@ozon.ru>
2025-04-28 10:33:19 +0300

    добавил checkers_function

---
+++ checkers/post_v1_account.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from hamcrest import (
+    assert_that,
+    starts_with,
+    all_of,
+    has_property,
+    has_properties,
+    equal_to,
+    instance_of,
+)
+
+
+class PostV1Account:
+    @staticmethod
+    def check_response_values(
+            response
+    ):
+        today = datetime.now().strftime('%Y-%m-%d')
+        assert_that(str(response.resource.registration), starts_with(today))
+        assert_that(
+            response, all_of(
+                has_property('resource', has_property('login', starts_with("allezov"))),
+                has_property('resource', has_property('registration', instance_of(datetime))),
+                has_property(
+                    'resource', has_properties(
+                        {
+                            "rating": has_properties(
+                                {
+                                    "enabled": equal_to(True),
+                                    "quality": equal_to(0),
+                                    "quantity": equal_to(0)
+                                }
+                            )
+                        }
+                    )
+                )
+            )
+        )
