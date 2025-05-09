4699cb410dc07e71fa53b3b436c5a6e9215f9350
LezovOzon <allezov@ozon.ru>
2025-05-09 08:42:14 +0300

    coverage

--- restclient/client.py
+++ restclient/client.py
@@ -6,6 +6,8 @@ from requests import (
 import structlog
 import uuid
 import curlify
+from swagger_coverage_py.request_schema_handler import RequestSchemaHandler
+from swagger_coverage_py.uri import URI
 
 from restclient.configuration import Configuration
 from restclient.utilities import allure_attach
@@ -85,6 +87,11 @@ class RestClient:
         rest_response = self.session.request(method=method, url=full_url, **kwargs)
 
         curl = curlify.to_curl(rest_response.request)
+
+        uri = URI(host=self.host, base_path="", unformatted_path=path, uri_params=kwargs.get('params'))
+        RequestSchemaHandler(
+            uri, method.lower(), rest_response, kwargs
+        ).write_schema()
         print(curl)
 
         log.msg(
